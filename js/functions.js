c5a9fd58562180159c27401af3628b935f361a0a
Danil Mikhailov <danil@altlinux.org>
2016-07-07 17:51:00 +0300

    Index: vertical centered, fixed loading

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,3 @@
-
-
 var getUrlParameter = function getUrlParameter(sParam) {
     var sPageURL = decodeURIComponent(window.location.search.substring(1)),
         sURLVariables = sPageURL.split('&'),
@@ -21,32 +19,36 @@ function changeUrl(url){
 }
 
 function setGetParametr(){
-	    if (getUrlParameter('s')){
-		$('#search').val(getUrlParameter('s'));
-		getBookList(getUrlParameter('s'));
-	    }
+    if ($('#search').val()){
+        changeUrl("?s=" + $('#search').val());
+    }
 }
 
-	function getBookList(bookName){
-		if ($('#search').val()){
-		    changeUrl("?s=" + $('#search').val());
-		}
-		$("#booklist").html("");
-		$body = $('#booklist');
-		$.get("php/GetBooks.php?t=" + $('#search').val(), function(data, status){
-	                $body.addClass("modal");
-			books = JSON.parse(data);
-			for(var i=0;i < books.length;i++){
-			     var current_book_title = '<div class="pure-menu pure-menu-horizontal"><a class="pure-menu-heading pure-menu-link" href="/b/'+ books[i].BookId + '/read">' + books[i].Title + '</a></div>';
-			     var author = '<li class="pure-menu-item"><a href="/a/' + books[i].AvtorId +'" class="pure-menu-link">' + books[i].FirstName + ' ' + books[i].LastName + '</a></li>';
-			     var read =  '<li class="pure-menu-item"><a href="/b/' + books[i].BookId + '/read" class="pure-menu-link">читать</a></li>';
-			     var epub =  '<li class="pure-menu-item"><a href="/b/' + books[i].BookId + '/epub" class="pure-menu-link">epub</a></li>';
-			     var mobi =  '<li class="pure-menu-item"><a href="/b/' + books[i].BookId + '/mobi" class="pure-menu-link">mobi</a></li>';
-			     var fb2 =  '<li class="pure-menu-item"><a href="/b/' + books[i].BookId + '/fb2" class="pure-menu-link">fb2</a></li>';
-			     var current_book = '<ul class="pure-menu-list">' + author + fb2 + epub + mobi + '</ul>';
+function setSearchString(){
+    if (getUrlParameter('s')){
+	$('#search').val(getUrlParameter('s'));
+	getBookList(getUrlParameter('s'));
+    }
+}
 
-		             $(current_book_title).appendTo('#booklist').append(current_book);
-			}
-	                $body.removeClass("modal");
-        	});
-        }
+function getBookList(bookName){
+    setGetParametr();
+    $("#booklist").html("");
+    $('#loading').addClass("loading");
+    $(".search_out").css("height", "1%"); //move up search
+    $.get("php/GetBooks.php?t=" + $('#search').val(), function(data, status){
+        $('#loading').removeClass("loading");
+	$(".search_out").css("position", "static");
+	books = JSON.parse(data);
+	for(var i=0;i < books.length;i++){
+	     var current_book_title = '<div class="pure-menu pure-menu-horizontal"><a class="pure-menu-heading pure-menu-link" href="/b/'+ books[i].BookId + '/read">' + books[i].Title + '</a></div>';
+	     var author = '<li class="pure-menu-item"><a href="/a/' + books[i].AvtorId +'" class="pure-menu-link">' + books[i].FirstName + ' ' + books[i].LastName + '</a></li>';
+	     var read =  '<li class="pure-menu-item"><a href="/b/' + books[i].BookId + '/read" class="pure-menu-link">читать</a></li>';
+	     var epub =  '<li class="pure-menu-item"><a href="/b/' + books[i].BookId + '/epub" class="pure-menu-link">epub</a></li>';
+	     var mobi =  '<li class="pure-menu-item"><a href="/b/' + books[i].BookId + '/mobi" class="pure-menu-link">mobi</a></li>';
+	     var fb2 =  '<li class="pure-menu-item"><a href="/b/' + books[i].BookId + '/fb2" class="pure-menu-link">fb2</a></li>';
+	     var current_book = '<ul class="pure-menu-list">' + author + fb2 + epub + mobi + '</ul>';
+             $(current_book_title).appendTo('#booklist').append(current_book);
+	}
+    });
+}
