cd49eb61205b306ce101c62be72821d6b08dae28
Danil Mikhailov <danil@altlinux.org>
2016-07-12 17:40:49 +0300

    Index: redesign with table

--- js/functions.js
+++ js/functions.js
@@ -31,9 +31,23 @@ function setSearchString(){
     }
 }
 
+function addLi(item){
+    var li = '<li class="pure-menu-item">';
+    var lie = '</li>'
+     
+    return li + item + lie;
+}
+
+function addTd(item){
+    var td = '<td>';
+    var tde = '</td>'
+     
+    return td + item + tde;
+}
+
 function getBookList(bookName){
     setGetParametr();
-    $("#booklist").html("");
+    $("#booktable").html(""); //clear table
     $('#loading').addClass("loading");
     $(".search_out").css("height", "1%"); //move up search
     $.get("php/GetBooks.php?t=" + $('#search').val(), function(data, status){
@@ -41,14 +55,16 @@ function getBookList(bookName){
 	$(".search_out").css("position", "static");
 	books = JSON.parse(data);
 	for(var i=0;i < books.length;i++){
-	     var current_book_title = '<div align="center" class="pure-menu pure-menu-horizontal"><a class="pure-menu-heading pure-menu-link" href="/b/'+ books[i].BookId + '/read">' + books[i].Title + '</a></div>';
-	     var author = '<li class="pure-menu-item"><a href="/a/' + books[i].AvtorId +'" class="pure-menu-link">' + books[i].FirstName + ' ' + books[i].LastName + '</a></li>';
-	     var read =  '<li class="pure-menu-item"><a href="/b/' + books[i].BookId + '/read" class="pure-menu-link">читать</a></li>';
-	     var epub =  '<li class="pure-menu-item"><a href="/b/' + books[i].BookId + '/epub" class="pure-menu-link">epub</a></li>';
-	     var mobi =  '<li class="pure-menu-item"><a href="/b/' + books[i].BookId + '/mobi" class="pure-menu-link">mobi</a></li>';
-	     var fb2 =  '<li class="pure-menu-item"><a href="/b/' + books[i].BookId + '/fb2" class="pure-menu-link">fb2</a></li>';
-	     var current_book = '<ul class="pure-menu-list">' + author + fb2 + epub + mobi + '</ul>';
-             $(current_book_title).appendTo('#booklist').append(current_book);
+	     var current = '<tr></tr>';
+	     var title = '<a class="pure-menu-heading pure-menu-link" style="height:100%;width:96%" href="/b/'+ books[i].BookId + '/read">' + books[i].Title + '</a>';
+	     var author = '<a href="/a/' + books[i].AvtorId +'" class="pure-menu-link">' + books[i].FirstName + ' ' + books[i].LastName + '</a>';
+	     var read =  '<a href="/b/' + books[i].BookId + '/read" class="pure-menu-link">читать</a>';
+	     var epub =  '<a href="/b/' + books[i].BookId + '/epub" class="pure-menu-link">epub</a>';
+	     var mobi =  '<a href="/b/' + books[i].BookId + '/mobi" class="pure-menu-link">mobi</a>';
+	     var fb2 =  '<a href="/b/' + books[i].BookId + '/fb2" class="pure-menu-link">fb2</a>';
+	     var list = '<ul class="pure-menu-list">' + '</ul>';
+	     var current_book = addTd(title) + addTd(author) + addTd(fb2) + addTd(epub) + addTd(mobi);
+             $(current).appendTo('#booktable').append(current_book);
 	}
     });
 }
