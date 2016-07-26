05e2080e358292403086058de1616e8d8fd56731
Danil Mikhailov <danil@altlinux.org>
2016-07-27 01:45:46 +0300

    SQL: add to rank middle and first name

--- php/GetBooks.php
+++ php/GetBooks.php
@@ -4,26 +4,34 @@ include './DbFunctions.php';
 class Books extends DbFunction{
 
 function get_books($string){
-        $query = "	SELECT BookId, Title, AvtorId, FirstName, MiddleName, LastName, SUM(MatchTitle), SUM(MatchLastName), SUM(MatchTitle) + SUM(MatchLastName)
+        $query = "	SELECT BookId, Title, AvtorId, FirstName, MiddleName, LastName, 
+    			    SUM(MatchTitle), SUM(MatchLastName), SUM(MatchFirstName), SUM(MatchMiddleName),
+    			    SUM(MatchTitle) + SUM(MatchLastName) + SUM(MatchFirstName) + SUM(MatchMiddleName)
 			FROM (
 			    SELECT BookId, Title, AvtorId, FirstName, MiddleName, LastName, 
-				MATCH (Title) AGAINST ('{$string}') as MatchTitle, 0 AS MatchLastName
+				MATCH (Title) AGAINST ('{$string}') as MatchTitle, 0 AS MatchLastName, 0 AS MatchMiddleName, 0 AS MatchFirstName
 			    FROM libbook 
 				JOIN libavtor USING(BookId)
 				JOIN libavtorname USING(AvtorId)
 			    WHERE (MATCH (Title) AGAINST ('{$string}*' IN BOOLEAN MODE))
+				AND Deleted = 0
 
 		            UNION
 
 			    SELECT BookId, Title, AvtorId, FirstName, MiddleName, LastName,
-				0 AS MatchTitle, MATCH (LastName) AGAINST ('{$string}') as MatchLastName
+				0 AS MatchTitle, 
+				MATCH (LastName) AGAINST ('{$string}') as MatchLastName,
+				MATCH (FirstName) AGAINST ('{$string}') as MatchFirstName,
+				MATCH (MiddleName) AGAINST ('{$string}') as MatchMiddleName
 			    FROM libbook 
 				JOIN libavtor USING(BookId)
 				JOIN libavtorname USING(AvtorId)
-			    WHERE MATCH (LastName) AGAINST ('{$string}*' IN BOOLEAN MODE) AND (Deleted = 0 AND Lang = 'ru' AND FileType = 'fb2')
+			    WHERE MATCH (LastName) AGAINST ('{$string}*' IN BOOLEAN MODE) 
+				AND (Lang = 'ru' AND FileType = 'fb2')
+
 			) AS books
 			GROUP BY BookId, Title, AvtorId, FirstName, MiddleName, LastName
-			ORDER BY SUM(MatchTitle) + SUM(MatchLastName) DESC, LastName, Title
+			ORDER BY SUM(MatchTitle) + SUM(MatchLastName) + SUM(MatchFirstName) + SUM(MatchMiddleName) DESC, LastName, Title
 			LIMIT 200;";
         return $this->query_to_json($query);
 }
@@ -36,4 +44,26 @@ if (strcmp("{$argv[1]}", "get_books") == 0){
 
 echo ($books->get_books($_GET['t']));
 
+/*
+		            UNION
+
+			    SELECT BookId, Title, AvtorId, FirstName, MiddleName, LastName,
+				0 AS MatchTitle, MATCH (FirstName) AGAINST ('{$string}') as MatchFirstName
+			    FROM libbook 
+				JOIN libavtor USING(BookId)
+				JOIN libavtorname USING(AvtorId)
+			    WHERE MATCH (FirstName) AGAINST ('{$string}*' IN BOOLEAN MODE) 
+				AND (Lang = 'ru' AND FileType = 'fb2')
+
+		            UNION
+
+			    SELECT BookId, Title, AvtorId, FirstName, MiddleName, LastName,
+				0 AS MatchTitle, MATCH (MiddleName) AGAINST ('{$string}') as MatchMiddleName
+			    FROM libbook 
+				JOIN libavtor USING(BookId)
+				JOIN libavtorname USING(AvtorId)
+			    WHERE MATCH (MiddleName) AGAINST ('{$string}*' IN BOOLEAN MODE) 
+				AND (Lang = 'ru' AND FileType = 'fb2')
+
+*/
 ?>
--- sql/db_index_creation.sql
+++ sql/db_index_creation.sql
@@ -6,3 +6,5 @@ ALTER TABLE `libavtorname` ADD INDEX `avtorlastname` (`LastName`);
 --add full text indexing
 CREATE FULLTEXT INDEX libbook_title_full_text_idx ON libbook(Title);
 CREATE FULLTEXT INDEX libavtorname_lastname_full_text_idx ON libavtorname(LastName);
+CREATE FULLTEXT INDEX libavtorname_middlename_full_text_idx ON libavtorname(MiddleName);
+CREATE FULLTEXT INDEX libavtorname_firstname_full_text_idx ON libavtorname(FirstName);
